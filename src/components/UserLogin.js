b521aaf004fa121c7c514e5f345480e6aeec89c2
unknown <caiiiok123@gmail.com>
2022-06-08 17:31:19 +0300

    reecapcha

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -40,7 +40,7 @@ const UserLogin = () => {
               Увійти
             </button>
             <div className="recaptcha">
-              <ReCAPTCHA sitekey="6LeocHIdAAAAAGu-IlshK7DBK20WXip_ccSgooKm" onChange={onChange} />
+              <ReCAPTCHA sitekey="6LfM1vQfAAAAAMQ842tpjo5_rzJXvYpXe4_3xRv8" onChange={onChange} />
             </div>
           </form>
         </div>
